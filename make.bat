29e255cf3cfb1682a658cd0cbca4f62eae024b08
LWY <668915+laxa88@users.noreply.github.com>
2021-12-24 23:03:49 +0900

    add load-tile code with custom spritesheet

--- make.bat
+++ make.bat
@@ -1,7 +1,11 @@
-set filename=hello-world
+@REM Silences the commands below
+@echo off
+
+set filename=02-load-tile
 set toolpath=.\tools\rgbds-0.5.1-win32\
 set binpath=.\bin\
 
+echo ### Compiling file: %filename%
 %toolpath%rgbasm -L -o %binpath%%filename%.o %filename%.asm
 %toolpath%rgblink -o %binpath%%filename%.gb %binpath%%filename%.o
 %toolpath%rgbfix -v -p 0xFF %binpath%%filename%.gb
