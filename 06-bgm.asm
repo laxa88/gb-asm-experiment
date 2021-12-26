c21ad399de3d9fd6264b7b59fff169e6d3c3a146
LWY <668915+laxa88@users.noreply.github.com>
2021-12-26 21:34:26 +0900

    add code from example 5

--- 06-bgm.asm
+++ 06-bgm.asm
@@ -4,15 +4,20 @@
 INCLUDE "include/hardware.inc"
 INCLUDE "include/util.asm"
 
-; Reserved for OAM data
+; Reserved address for OAM data
 DEF RAM_OAM EQU $c100       ; Reserve this address for OAM data
 DEF RAM_OAM_END EQU $c1a0   ; 40 sprites * 4 bytes = $a0 (60)
 
+; Local variable definitions
+DEF inputs EQU $cfff
+DEF crosshairX EQU $cffe
+DEF crosshairY EQU $cffd
+
 SECTION "RST 0 - 7", ROM0[$00]
   ds $40 - @, 0      ; pad zero from @ (current address)
+  ; nop
 
 SECTION "VBlank interrupt", ROM0[$40]
-  ; call PlayMusic
   jp _HRAM ; Vblank
 
 SECTION "LCD-Stat interrupt", ROM0[$48]
@@ -20,6 +25,7 @@ SECTION "LCD-Stat interrupt", ROM0[$48]
 
 SECTION "Timer interrupt", ROM0[$50]
   call PlayMusic
+  call MoveCrosshair
   reti
 
 SECTION "Serial interrupt", ROM0[$58]
@@ -70,12 +76,37 @@ WaitVBlank:
   xor a
   ld [rLCDC], a
 
+  ; Copy the tile data
+  ld de, Tiles
+  ld hl, $9000
+  ld bc, TilesEnd - Tiles
+  call CopyTiles
+
+  ld de, MySpriteSheet
+  ld hl, $8800
+  ld bc, MySpriteSheetEnd - MySpriteSheet
+  call CopyTiles
+
+  ; Copy the tilemap
+  ld de, Tilemap
+  ld hl, $9800
+  ld bc, TilemapEnd - Tilemap
+  call CopyTilemap
+
   ; Turn the LCD on
   ld a, LCDCF_ON | LCDCF_BGON | LCDCF_OBJON | LCDCF_BG8800
   ld [rLCDC], a
 
-  ld a, 0
-  ld [rTMA], a                    ; Reset timer by this much every clock
+  ; During the first (blank) frame, initialize display registers
+  ld a, %11100100
+  ld [rBGP], a    ; bg palette
+  ld [rOBP0], a   ; obj 0 palette
+  cpl ; invert a
+  ld [rOBP1], a   ; obj 1 palette (usually inverted of obj 0)
+
+  ; Init timer
+  ld a, 0                         ; Reset timer by this much every clock
+  ld [rTMA], a
   ld a, TACF_16KHZ | TACF_START   ; 00, 11, 10, 01 (slowest to fastest)
   ld [rTAC], a                    ; Timer control, b2 = start timer, b0/1 = clock speed
 
@@ -85,6 +116,12 @@ WaitVBlank:
   ld a, IEF_VBLANK | IEF_TIMER
   ld [rIE], a
   ei
+
+  ; Set initial sprite
+  ld a, $58
+  ld [crosshairX], a
+  ld a, $30
+  ld [crosshairY], a
 
   ; Init sound
   ld a, AUDENA_ON     ; enable sounds
@@ -99,7 +136,135 @@ WaitVBlank:
   call hUGE_init
 
 Loop:
+  call ReadInput
+  call DrawCrosshair
   jp Loop
+
+SECTION "Global functions", ROM0
+
+; Reads inputs and puts them in register A, where:
+; - hi-nibble is dpad (7654 = Down, Up, Left, Right)
+; - lo-nibble is buttons (3210 = Start, Select, B, A)
+; - 1 = pressed, 0 = not pressed
+; e.g. 00010010 = RIGHT and B are pressed
+ReadInput:
+  push af
+  push bc
+    ; Reset all input states before checking every loop
+    xor a
+    ld [rP1], a
+    ; Reference: https://gbdev.io/pandocs/Joypad_Input.html
+    ; invert to 11101111 to select bit-4 (dpad)
+    ld a, ~P1F_4
+    ld [rP1], a
+    ld a, [rP1]       ; read values (Remember: 0 = selected!)
+    or %11110000      ; pad hi-nibble first
+    rlca
+    rlca
+    rlca
+    rlca              ; store values to 7654 (dpad nibble)
+    ld b, a           ; save to B
+    ; invert to 11011111 to select bit-5 (buttons)
+    ld a, ~P1F_5
+    ld a, [rP1]       ; read values (Remember: 0 = selected!)
+    or %11110000      ; ignore upper nibbles
+    and b             ; merge with dpad nibble
+    ld [inputs], a
+  pop bc
+  pop af
+  ret
+
+MoveCrosshair:
+  push bc
+    ; Load XY pos
+    push af
+      ld a, [crosshairX]
+      ld b, a
+      ld a, [crosshairY]
+      ld c, a
+    pop af
+
+    ; Update XY pos
+    push af
+.checkRight
+      ld a, [inputs]
+      and %00010000
+      cp %00010000
+      jr z, .checkLeft
+      inc b
+.checkLeft
+      ld a, [inputs]
+      and %00100000
+      cp %00100000
+      jr z, .checkUp
+      dec b
+.checkUp
+      ld a, [inputs]
+      and %01000000
+      cp %01000000
+      jr z, .checkDown
+      dec c
+.checkDown
+      ld a, [inputs]
+      and %10000000
+      cp %10000000
+      jr z, .done
+      inc c
+.done
+    pop af
+    ; Save XY pos
+    push af
+      ld a, b
+      ld [crosshairX], a
+      ld a, c
+      ld [crosshairY], a
+    pop af
+  pop bc
+  ret
+
+DrawCrosshair:
+  push af
+  push bc
+    ld a, [crosshairX]
+    ld b, a
+    ld a, [crosshairY]
+    ld c, a
+    push bc
+      ld e, $8c       ; tile index
+      ld h, 0         ; tile details (palette, etc.)
+      ld a, 0         ; OBJ 0
+      call SetSprite  ; top left
+    pop bc
+    push bc
+      ld a, b
+      add 8
+      ld b, a
+      ld e, $8e
+      ld a, 1         ; OBJ 1
+      call SetSprite  ; top right
+    pop bc
+    push bc
+      ld a, c
+      add 8
+      ld c, a
+      ld e, $8d
+      ld a, 2         ; OBJ 2
+      call SetSprite  ; bottom left
+    pop bc
+    push bc
+      ld a, b
+      add 8
+      ld b, a
+      ld a, c
+      add 8
+      ld c, a
+      ld e, $8f
+      ld a, 3         ; OBJ 2
+      call SetSprite  ; bottom left
+    pop bc
+  pop bc
+  pop af
+  ret
 
 PlayMusic:
   push af
@@ -131,3 +296,155 @@ DMACopyWait:
   pop af
   reti
 DMACopyEnd:
+
+CopyTiles:
+  ld a, [de]
+  ld [hli], a
+  inc de
+  dec bc
+  ld a, b
+  or a, c
+  jp nz, CopyTiles
+  ret
+
+CopyTilemap:
+  ld a, [de]
+  ld [hli], a
+  inc de
+  dec bc
+  ld a, b
+  or a, c
+  jp nz, CopyTilemap
+  ret
+
+; Set sprite
+; - A = Sprite number (0 to 39)
+; - BC = X,Y
+; - E = Tile index from VRAM
+; - H = Palette, etc
+; - Note: On GB, XY needs to be 8,16 to get top-left corner of screen (?)
+SetSprite:
+  push af
+    ; rotate A left, copy bit-7 to Carry and bit-0
+    rlca                  ; 4 bytes per sprite
+    rlca
+    push hl
+    push de
+      push hl
+        ld hl, RAM_OAM    ; Cache to be copied via DMA
+        ld l, a           ; address for selected sprite
+        ld a, c           ; Y
+        ldi [hl], a
+        ld a, b           ; X
+        ldi [hl], a
+        ld a, e           ; tile
+        ldi [hl], a
+      pop hl
+      ld a, d             ; attributes
+      ldi [hl], a
+    pop de
+    pop hl
+  pop af
+  ret
+
+SECTION "Tile data", ROM0
+
+Tiles:
+  db $00,$ff, $00,$ff, $00,$ff, $00,$ff, $00,$ff, $00,$ff, $00,$ff, $00,$ff
+  db $00,$ff, $00,$80, $00,$80, $00,$80, $00,$80, $00,$80, $00,$80, $00,$80
+  db $00,$ff, $00,$7e, $00,$7e, $00,$7e, $00,$7e, $00,$7e, $00,$7e, $00,$7e
+  db $00,$ff, $00,$01, $00,$01, $00,$01, $00,$01, $00,$01, $00,$01, $00,$01
+  db $00,$ff, $00,$00, $00,$00, $00,$00, $00,$00, $00,$00, $00,$00, $00,$00
+  db $00,$ff, $00,$7f, $00,$7f, $00,$7f, $00,$7f, $00,$7f, $00,$7f, $00,$7f
+  db $00,$ff, $03,$fc, $00,$f8, $00,$f0, $00,$e0, $20,$c0, $00,$c0, $40,$80
+  db $00,$ff, $c0,$3f, $00,$1f, $00,$0f, $00,$07, $04,$03, $00,$03, $02,$01
+  db $00,$80, $00,$80, $7f,$80, $00,$80, $00,$80, $7f,$80, $7f,$80, $00,$80
+  db $00,$7e, $2a,$7e, $d5,$7e, $2a,$7e, $54,$7e, $ff,$00, $ff,$00, $00,$00
+  db $00,$01, $00,$01, $ff,$01, $00,$01, $01,$01, $fe,$01, $ff,$01, $00,$01
+  db $00,$80, $80,$80, $7f,$80, $80,$80, $00,$80, $ff,$80, $7f,$80, $80,$80
+  db $00,$7f, $2a,$7f, $d5,$7f, $2a,$7f, $55,$7f, $ff,$00, $ff,$00, $00,$00
+  db $00,$ff, $aa,$ff, $55,$ff, $aa,$ff, $55,$ff, $fa,$07, $fd,$07, $02,$07
+  db $00,$7f, $2a,$7f, $d5,$7f, $2a,$7f, $55,$7f, $aa,$7f, $d5,$7f, $2a,$7f
+  db $00,$ff, $80,$ff, $00,$ff, $80,$ff, $00,$ff, $80,$ff, $00,$ff, $80,$ff
+  db $40,$80, $00,$80, $7f,$80, $00,$80, $00,$80, $7f,$80, $7f,$80, $00,$80
+  db $00,$3c, $02,$7e, $85,$7e, $0a,$7e, $14,$7e, $ab,$7e, $95,$7e, $2a,$7e
+  db $02,$01, $00,$01, $ff,$01, $00,$01, $01,$01, $fe,$01, $ff,$01, $00,$01
+  db $00,$ff, $80,$ff, $50,$ff, $a8,$ff, $50,$ff, $a8,$ff, $54,$ff, $a8,$ff
+  db $7f,$80, $7f,$80, $7f,$80, $7f,$80, $7f,$80, $7f,$80, $7f,$80, $7f,$80
+  db $ff,$00, $ff,$00, $ff,$00, $ab,$7e, $d5,$7e, $ab,$7e, $d5,$7e, $ab,$7e
+  db $ff,$01, $fe,$01, $ff,$01, $fe,$01, $ff,$01, $fe,$01, $ff,$01, $fe,$01
+  db $7f,$80, $ff,$80, $7f,$80, $ff,$80, $7f,$80, $ff,$80, $7f,$80, $ff,$80
+  db $ff,$00, $ff,$00, $ff,$00, $aa,$7f, $d5,$7f, $aa,$7f, $d5,$7f, $aa,$7f
+  db $f8,$07, $f8,$07, $f8,$07, $80,$ff, $00,$ff, $aa,$ff, $55,$ff, $aa,$ff
+  db $7f,$80, $7f,$80, $7f,$80, $7f,$80, $7f,$80, $ff,$80, $7f,$80, $ff,$80
+  db $d5,$7f, $aa,$7f, $d5,$7f, $aa,$7f, $d5,$7f, $aa,$7f, $d5,$7f, $aa,$7f
+  db $d5,$7e, $ab,$7e, $d5,$7e, $ab,$7e, $d5,$7e, $ab,$7e, $d5,$7e, $eb,$3c
+  db $54,$ff, $aa,$ff, $54,$ff, $aa,$ff, $54,$ff, $aa,$ff, $54,$ff, $aa,$ff
+  db $7f,$80, $7f,$80, $7f,$80, $7f,$80, $7f,$80, $7f,$80, $7f,$80, $00,$ff
+  db $d5,$7e, $ab,$7e, $d5,$7e, $ab,$7e, $d5,$7e, $ab,$7e, $d5,$7e, $2a,$ff
+  db $ff,$01, $fe,$01, $ff,$01, $fe,$01, $ff,$01, $fe,$01, $ff,$01, $80,$ff
+  db $7f,$80, $ff,$80, $7f,$80, $ff,$80, $7f,$80, $ff,$80, $7f,$80, $aa,$ff
+  db $ff,$00, $ff,$00, $ff,$00, $ff,$00, $ff,$00, $ff,$00, $ff,$00, $2a,$ff
+  db $ff,$01, $fe,$01, $ff,$01, $fe,$01, $fe,$01, $fe,$01, $fe,$01, $80,$ff
+  db $7f,$80, $ff,$80, $7f,$80, $7f,$80, $7f,$80, $7f,$80, $7f,$80, $00,$ff
+  db $fe,$01, $fe,$01, $fe,$01, $fe,$01, $fe,$01, $fe,$01, $fe,$01, $80,$ff
+  db $3f,$c0, $3f,$c0, $3f,$c0, $1f,$e0, $1f,$e0, $0f,$f0, $03,$fc, $00,$ff
+  db $fd,$03, $fc,$03, $fd,$03, $f8,$07, $f9,$07, $f0,$0f, $c1,$3f, $82,$ff
+  db $55,$ff, $2a,$7e, $54,$7e, $2a,$7e, $54,$7e, $2a,$7e, $54,$7e, $00,$7e
+  db $01,$ff, $00,$01, $01,$01, $00,$01, $01,$01, $00,$01, $01,$01, $00,$01
+  db $54,$ff, $ae,$f8, $50,$f0, $a0,$e0, $60,$c0, $80,$c0, $40,$80, $40,$80
+  db $55,$ff, $00,$00, $00,$00, $00,$00, $00,$00, $00,$00, $00,$00, $00,$00
+  db $55,$ff, $6a,$1f, $05,$0f, $02,$07, $05,$07, $02,$03, $03,$01, $02,$01
+  db $54,$ff, $80,$80, $00,$80, $80,$80, $00,$80, $80,$80, $00,$80, $00,$80
+  db $55,$ff, $2a,$1f, $0d,$07, $06,$03, $01,$03, $02,$01, $01,$01, $00,$01
+  db $55,$ff, $2a,$7f, $55,$7f, $2a,$7f, $55,$7f, $2a,$7f, $55,$7f, $00,$7f
+  db $55,$ff, $aa,$ff, $55,$ff, $aa,$ff, $55,$ff, $aa,$ff, $55,$ff, $00,$ff
+  db $15,$ff, $00,$00, $00,$00, $00,$00, $00,$00, $00,$00, $00,$00, $00,$00
+  db $55,$ff, $6a,$1f, $0d,$07, $06,$03, $01,$03, $02,$01, $03,$01, $00,$01
+  db $54,$ff, $a8,$ff, $54,$ff, $a8,$ff, $50,$ff, $a0,$ff, $40,$ff, $00,$ff
+  db $00,$7e, $2a,$7e, $d5,$7e, $2a,$7e, $54,$7e, $ab,$76, $dd,$66, $22,$66
+  db $00,$7c, $2a,$7e, $d5,$7e, $2a,$7e, $54,$7c, $ff,$00, $ff,$00, $00,$00
+  db $00,$01, $00,$01, $ff,$01, $02,$01, $07,$01, $fe,$03, $fd,$07, $0a,$0f
+  db $00,$7c, $2a,$7e, $d5,$7e, $2a,$7e, $54,$7e, $ab,$7e, $d5,$7e, $2a,$7e
+  db $00,$ff, $a0,$ff, $50,$ff, $a8,$ff, $54,$ff, $a8,$ff, $54,$ff, $aa,$ff
+  db $dd,$62, $bf,$42, $fd,$42, $bf,$40, $ff,$00, $ff,$00, $f7,$08, $ef,$18
+  db $ff,$00, $ff,$00, $ff,$00, $ab,$7c, $d5,$7e, $ab,$7e, $d5,$7e, $ab,$7e
+  db $f9,$07, $fc,$03, $fd,$03, $fe,$01, $ff,$01, $fe,$01, $ff,$01, $fe,$01
+  db $d5,$7e, $ab,$7e, $d5,$7e, $ab,$7e, $d5,$7e, $ab,$7e, $d5,$7e, $ab,$7c
+  db $f7,$18, $eb,$1c, $d7,$3c, $eb,$3c, $d5,$3e, $ab,$7e, $d5,$7e, $2a,$ff
+  db $ff,$01, $fe,$01, $ff,$01, $fe,$01, $ff,$01, $fe,$01, $ff,$01, $a2,$ff
+  db $7f,$c0, $bf,$c0, $7f,$c0, $bf,$e0, $5f,$e0, $af,$f0, $57,$fc, $aa,$ff
+  db $ff,$01, $fc,$03, $fd,$03, $fc,$03, $f9,$07, $f0,$0f, $c1,$3f, $82,$ff
+  db $55,$ff, $2a,$ff, $55,$ff, $2a,$ff, $55,$ff, $2a,$ff, $55,$ff, $00,$ff
+  db $45,$ff, $a2,$ff, $41,$ff, $82,$ff, $41,$ff, $80,$ff, $01,$ff, $00,$ff
+  db $54,$ff, $aa,$ff, $54,$ff, $aa,$ff, $54,$ff, $aa,$ff, $54,$ff, $00,$ff
+  db $15,$ff, $2a,$ff, $15,$ff, $0a,$ff, $15,$ff, $0a,$ff, $01,$ff, $00,$ff
+  db $01,$ff, $80,$ff, $01,$ff, $80,$ff, $01,$ff, $80,$ff, $01,$ff, $00,$ff
+TilesEnd:
+
+SECTION "Tilemap", ROM0
+
+Tilemap:
+  db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00,  0,0,0,0,0,0,0,0,0,0,0,0
+  db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00,  0,0,0,0,0,0,0,0,0,0,0,0
+  db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00,  0,0,0,0,0,0,0,0,0,0,0,0
+  db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00,  0,0,0,0,0,0,0,0,0,0,0,0
+  db $00, $00, $01, $02, $03, $01, $04, $03, $01, $05, $00, $01, $05, $00, $06, $04, $07, $00, $00, $00,  0,0,0,0,0,0,0,0,0,0,0,0
+  db $00, $00, $08, $09, $0a, $0b, $0c, $0d, $0b, $0e, $0f, $08, $0e, $0f, $10, $11, $12, $13, $00, $00,  0,0,0,0,0,0,0,0,0,0,0,0
+  db $00, $00, $14, $15, $16, $17, $18, $19, $1a, $1b, $0f, $14, $1b, $0f, $14, $1c, $16, $1d, $00, $00,  0,0,0,0,0,0,0,0,0,0,0,0
+  db $00, $00, $1e, $1f, $20, $21, $22, $23, $24, $22, $25, $1e, $22, $25, $26, $22, $27, $1d, $00, $00,  0,0,0,0,0,0,0,0,0,0,0,0
+  db $00, $00, $01, $28, $29, $2a, $2b, $2c, $2d, $2b, $2e, $2d, $2f, $30, $2d, $31, $32, $33, $00, $00,  0,0,0,0,0,0,0,0,0,0,0,0
+  db $00, $00, $08, $34, $0a, $0b, $11, $0a, $0b, $35, $36, $0b, $0e, $0f, $08, $37, $0a, $38, $00, $00,  0,0,0,0,0,0,0,0,0,0,0,0
+  db $00, $00, $14, $39, $16, $17, $1c, $16, $17, $3a, $3b, $17, $1b, $0f, $14, $3c, $16, $1d, $00, $00,  0,0,0,0,0,0,0,0,0,0,0,0
+  db $00, $00, $1e, $3d, $3e, $3f, $22, $27, $21, $1f, $20, $21, $22, $25, $1e, $22, $40, $1d, $00, $00,  0,0,0,0,0,0,0,0,0,0,0,0
+  db $00, $00, $00, $41, $42, $43, $44, $30, $33, $41, $45, $43, $41, $30, $43, $41, $30, $33, $00, $00,  0,0,0,0,0,0,0,0,0,0,0,0
+  db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00,  0,0,0,0,0,0,0,0,0,0,0,0
+  db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00,  0,0,0,0,0,0,0,0,0,0,0,0
+  db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00,  0,0,0,0,0,0,0,0,0,0,0,0
+  db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00,  0,0,0,0,0,0,0,0,0,0,0,0
+  db $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00,  0,0,0,0,0,0,0,0,0,0,0,0
+TilemapEnd:
+
+MySpriteSheet:
+  incbin "./sprite-sheet.bin"
+MySpriteSheetEnd:
