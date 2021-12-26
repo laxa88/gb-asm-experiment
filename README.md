742449d4ccbbc1545e6c7b1f48626166b8de22d2
LWY <668915+laxa88@users.noreply.github.com>
2021-12-26 21:35:52 +0900

    Sound (#4)
    * add debug symbol
    
    * fix input not reading buttons correctly
    
    * fix SetSprite logic
    
    * WIP add initial music
    
    * WIP add 2nd music test file
    
    * add readme
    
    * add tools
    
    * rename
    
    * fix music, but there are garbage on screen
    
    * remove code unrelated to sound
    
    * refactor
    
    * WIP don't play music
    
    * TMP fixed OAM garbage
    
    * archive previous asm file
    
    * rename file
    
    * fix garbage OAM issue
    
    * update make.bat
    
    * add code from example 5

---
+++ README.md
@@ -0,0 +1,16 @@
+# Experimental Game Boy Assembly Project
+
+Just a bunch of template code, teaching myself how to make a simple Game Boy game. My goal is to complete a game like "tic tac toe" which includes all the basic features that comprise a game:
+
+- Tiled background images
+- Sprite images with animations
+- Input
+- Music and SFX
+
+### Environment
+
+This project is developed using Visual Studio Code on a Windows machine.
+
+### Build
+
+Run `make.bat`, which should build and open the `.gb` game in BGB emulator.
--- make.bat
+++ make.bat
@@ -1,13 +1,23 @@
 @REM Silences the commands below
 @echo off
 
-set filename=05-input-move-sprite
-set toolpath=.\tools\rgbds-0.5.1-win32\
-set binpath=.\bin\
+set filename=06-bgm
+set musicname=pokemon_center
+set toolpath=tools\rgbds-0.5.1-win32\
+set binpath=bin\
 
-echo ### Compiling file: %filename%
-%toolpath%rgbasm -L -o %binpath%%filename%.o %filename%.asm
-%toolpath%rgblink -o %binpath%%filename%.gb %binpath%%filename%.o
+echo ### Compiling files: %filename%
+@REM https://rgbds.gbdev.io/docs/master/rgbasm.1
+@REM Assemble code into .obj files
+%toolpath%rgbasm -o %binpath%hUGEDriver.obj -i .. include/hUGEDriver.asm
+%toolpath%rgbasm -o %binpath%%musicname%.obj -i .. %musicname%.asm
+%toolpath%rgbasm -o %binpath%%filename%.obj -D SONG_DESCRIPTOR=%musicname% %filename%.asm
+
+echo ### Linking files...
+@REM https://rgbds.gbdev.io/docs/master/rgblink.1
+@REM Link all .obj files into a .gb file, BGB debugger .sym file
+%toolpath%rgblink -o %binpath%%filename%.gb -n %binpath%%filename%.sym %binpath%%filename%.obj %binpath%hUGEDriver.obj %binpath%%musicname%.obj
+
 %toolpath%rgbfix -v -p 0xFF %binpath%%filename%.gb
 
 echo ### Starting BGB...
