ea808d6e630af19a74f07f340c92d4583f03348d
LWY <668915+laxa88@users.noreply.github.com>
2021-12-26 21:21:39 +0900

    update make.bat

--- make.bat
+++ make.bat
@@ -2,20 +2,21 @@
 @echo off
 
 set filename=06-bgm
-set musicname=quasar
+set musicname=pokemon_center
 set toolpath=tools\rgbds-0.5.1-win32\
 set binpath=bin\
 
 echo ### Compiling files: %filename%
-@REM %toolpath%rgbasm -L -o %binpath%%filename%.o %filename%.asm
+@REM https://rgbds.gbdev.io/docs/master/rgbasm.1
+@REM Assemble code into .obj files
 %toolpath%rgbasm -o %binpath%hUGEDriver.obj -i .. include/hUGEDriver.asm
 %toolpath%rgbasm -o %binpath%%musicname%.obj -i .. %musicname%.asm
 %toolpath%rgbasm -o %binpath%%filename%.obj -D SONG_DESCRIPTOR=%musicname% %filename%.asm
 
-@REM %toolpath%rgblink -o %binpath%%filename%.gb -n %binpath%%filename%.sym %binpath%%filename%.o
-@REM %toolpath%rgblink -o %binpath%%filename%.gb %binpath%%filename%.obj %binpath%hUGEDriver.obj %binpath%%musicname%.obj
+echo ### Linking files...
+@REM https://rgbds.gbdev.io/docs/master/rgblink.1
+@REM Link all .obj files into a .gb file, BGB debugger .sym file
 %toolpath%rgblink -o %binpath%%filename%.gb -n %binpath%%filename%.sym %binpath%%filename%.obj %binpath%hUGEDriver.obj %binpath%%musicname%.obj
-@REM %toolpath%rgblink -n %binpath%%filename%.sym %binpath%%filename%.o %binpath%hUGEDriver.obj %binpath%%musicname%.obj
 
 %toolpath%rgbfix -v -p 0xFF %binpath%%filename%.gb
 
