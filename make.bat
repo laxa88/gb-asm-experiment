d577c069ad58fde1460f633f4eb5766e28d54b56
LWY <668915+laxa88@users.noreply.github.com>
2021-12-27 22:23:59 +0900

    exit when make has error

--- make.bat
+++ make.bat
@@ -10,15 +10,20 @@ echo ### Compiling files: %filename%
 @REM https://rgbds.gbdev.io/docs/master/rgbasm.1
 @REM Assemble code into .obj files
 %toolpath%rgbasm -o %binpath%hUGEDriver.obj -i .. include/hUGEDriver.asm
+if ERRORLEVEL 1 exit
 %toolpath%rgbasm -o %binpath%%musicname%.obj -i .. %musicname%.asm
+if ERRORLEVEL 1 exit
 %toolpath%rgbasm -o %binpath%%filename%.obj -D SONG_DESCRIPTOR=%musicname% %filename%.asm
+if ERRORLEVEL 1 exit
 
 echo ### Linking files...
 @REM https://rgbds.gbdev.io/docs/master/rgblink.1
 @REM Link all .obj files into a .gb file, BGB debugger .sym file
 %toolpath%rgblink -o %binpath%%filename%.gb -n %binpath%%filename%.sym %binpath%%filename%.obj %binpath%hUGEDriver.obj %binpath%%musicname%.obj
+if ERRORLEVEL 1 exit
 
 %toolpath%rgbfix -v -p 0xFF %binpath%%filename%.gb
+if ERRORLEVEL 1 exit
 
 echo ### Starting BGB...
 .\tools\bgb\bgb.exe %binpath%%filename%.gb
