b32462c12d6cd21666c25c353ac5f66234c1fac5
LWY <668915+laxa88@users.noreply.github.com>
2021-12-27 23:03:34 +0900

    Sfx (#5)
    * archive file 06
    
    * refactor update logic
    
    * fix input
    
    * add comment to hardware.inc
    
    * add SFX on press button B
    
    * optimise main loop
    
    * update comment
    
    * exit when make has error
    
    * rename macros
    
    * slow down SFX call
    
    * refactor rlca to swap

--- make.bat
+++ make.bat
@@ -1,7 +1,7 @@
 @REM Silences the commands below
 @echo off
 
-set filename=06-bgm
+set filename=07-sfx
 set musicname=pokemon_center
 set toolpath=tools\rgbds-0.5.1-win32\
 set binpath=bin\
@@ -10,15 +10,20 @@ echo ### Compiling files: %filename%
 @REM https://rgbds.gbdev.io/docs/master/rgbasm.1
 @REM Assemble code into .obj files
 %toolpath%rgbasm -o %binpath%hUGEDriver.obj -i .. include/hUGEDriver.asm
+if ERRORLEVEL 1 exit
 %toolpath%rgbasm -o %binpath%%musicname%.obj -i .. %musicname%.asm
+if ERRORLEVEL 1 exit
 %toolpath%rgbasm -o %binpath%%filename%.obj -D SONG_DESCRIPTOR=%musicname% %filename%.asm
+if ERRORLEVEL 1 exit
 
 echo ### Linking files...
 @REM https://rgbds.gbdev.io/docs/master/rgblink.1
 @REM Link all .obj files into a .gb file, BGB debugger .sym file
 %toolpath%rgblink -o %binpath%%filename%.gb -n %binpath%%filename%.sym %binpath%%filename%.obj %binpath%hUGEDriver.obj %binpath%%musicname%.obj
+if ERRORLEVEL 1 exit
 
 %toolpath%rgbfix -v -p 0xFF %binpath%%filename%.gb
+if ERRORLEVEL 1 exit
 
 echo ### Starting BGB...
 .\tools\bgb\bgb.exe %binpath%%filename%.gb
